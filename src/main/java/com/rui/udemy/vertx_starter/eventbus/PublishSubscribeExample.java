eaa0ed32ea90549b26964a453bb34c595487cfe5
LAPTOP-B60H1CKO\rui_l <rui_luis@outlook.com>
2021-11-08 14:24:38 +0000

    Publish Subscribe Example

---
+++ src/main/java/com/rui/udemy/vertx_starter/eventbus/PublishSubscribeExample.java
@@ -0,0 +1,53 @@
+package com.rui.udemy.vertx_starter.eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PublishSubscribeExample {
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new Publish());
+    vertx.deployVerticle(new SubscribeA());
+    vertx.deployVerticle(new SubscribeB());
+  }
+
+  static class Publish extends AbstractVerticle {
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.setPeriodic(1000, id -> {
+        vertx.eventBus().publish(Publish.class.getName(), "Hello world from Publish");
+      });
+    }
+
+  }
+
+  static class SubscribeA extends AbstractVerticle {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SubscribeA.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().consumer(Publish.class.getName(), message -> {
+        LOG.debug("Received message {} in {}", this.getClass(), message.body());
+      });
+    }
+  }
+
+  static class SubscribeB extends AbstractVerticle {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SubscribeB.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().consumer(Publish.class.getName(), message -> {
+        LOG.debug("Received message {} in {}", this.getClass(), message.body());
+      });
+    }
+  }
+}
